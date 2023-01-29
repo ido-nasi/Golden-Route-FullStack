f8b6869d9d52eb1c141ff9d45d60d7fde7cbb11e
ido-nasi <idoansi18@gmail.com>
2023-01-29 23:44:36 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ido-nasi/GoldenLane/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, "Hello world")
+	})
+
+	router.Run()
+}
